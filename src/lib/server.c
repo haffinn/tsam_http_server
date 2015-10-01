2267fe1794edd0f9de488d7292eab75319674b9b
rthor <ragnar.valgeirsson@gmail.com>
2015-10-01 19:40:17 +0000

    Serve an index file or 404

--- src/lib/server.c
+++ src/lib/server.c
@@ -7,13 +7,17 @@
 void server(session_t* session) 
 {
     char buffer[6000];
-    printf("Port %d\n", session->port);
+    gchar **lines, **tokens;
+    char verb[25], resource[255];
+    int connectFd;
+    FILE* file;
+
+    char *headerOk = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    char *headerFail = "HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n<h1>Not found</h1>";
 
     for (;;)
     {
-    	int connectFd = accept(session->socket_fd, NULL, NULL);
-
-    	if (connectFd < 0)
+    	if ((connectFd = accept(session->socket_fd, NULL, NULL)) < 0)
     	{
     		perror("Accept failed\n");
     		close(session->socket_fd);
@@ -21,7 +25,25 @@ void server(session_t* session)
     	}
 
         read(connectFd, buffer, 5999);
-        printf("[Request]:\n%s\n", buffer);
+
+        lines = g_strsplit(buffer, "\n", 3);
+        tokens = g_strsplit(lines[0], " ", 3);
+        strncpy(verb, tokens[0], 25);
+        strncpy(resource, tokens[1], 255);
+
+        file = fopen("htdocs/index.html", "r");
+
+        if (file != NULL && (!g_strcmp0(resource, "/") || !g_strcmp0(resource, "/index.html")))
+        {
+            fread(buffer, 5999, 1, file);
+            send(connectFd, headerOk, strlen(headerOk), 0);
+            send(connectFd, buffer, strlen(buffer), 0);
+        } 
+        else
+        {
+            send(connectFd, headerFail, strlen(headerFail), 0);
+        }
+        
 
     	if (shutdown(connectFd, SHUT_RDWR) == -1)
     	{
